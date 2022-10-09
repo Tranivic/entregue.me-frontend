0c0139d716296d5a694b28f20281d10e51214048
Victor <victor.trani1@gmail.com>
2022-10-09 18:54:20 -0300

    project's base structure created

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">Isso é um header</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./scss/global.scss";
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+const path = require("path");
+
+module.exports = {
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+    }
+  }
+};
