e4a914222d80bf323affefd7eb6e91b04bb5533d
Victor <victor.trani1@gmail.com>
2022-10-12 20:19:31 -0300

    Some files deleted, structure changed and header almost finished

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="header">
-    <div class="h-container">
-      <div class="level-left">Parte da logo</div>
-      <div class="level-right">Parte da pesquisa e Login</div>
+    <div class="superior">
+      <div class="logo-container">
+        <div class="info-container">
+          <img
+            class="logo"
+            src="https://res.cloudinary.com/amodeliveryimgs/image/upload/w_512,h_512,c_thumb/ldzvlmdkpbzbabtxc3qb.png"
+            alt="Logo image"
+          />
+
+          <div class="info">
+            <h1 class="title">Tojô</h1>
+            <div class="status">
+              <div class="ball"></div>
+              <h1 class="text">Fechado</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="search-container">
+        <div class="input-container">
+          <i class="fa fa-search"></i>
+          <input type="text" placeholder="Buscar produtos" />
+        </div>
+        <div class="line-divisor"></div>
+        <div class="btn-container">
+          <button>Fazer login ou cadastro</button>
+        </div>
+      </div>
     </div>
     <div class="navbar-menu"></div>
   </div>
@@ -15,6 +40,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "./scss/global.scss";
 </style>
