abd7aa1666dab3034ad5a873e8f7035da703a648
Victor <victor.trani1@gmail.com>
2022-10-17 20:16:13 -0300

    configuration to import variable.scss and carousel dependencies added

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,24 @@
+const path = require("path");
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-const path = require("path");
 
 module.exports = {
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
       patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+    }
+  },
+
+  //Responsable config for auto importa variables into vue components
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "~@/scss/setup/_variables.scss"; @import "~@/scss/setup/_mixins.scss";'
+      }
     }
   }
 };
